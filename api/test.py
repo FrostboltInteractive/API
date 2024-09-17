041ba51424d2b721e736008ddc52e2cff5159d71
vinnie <Vrinella4@gmail.com>
2024-09-17 14:19:01 -0400

    s

--- api/test.py
+++ api/test.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from getServerList import getServerList  # Correct import
+#from getServerList import getServerList  # Correct import
 
 
 app = Flask(__name__)
@@ -11,3 +11,16 @@ def home():
 @app.route('/getServerList')
 def handleGetServerList():
     return jsonify(getServerList())
+
+def getServerList():
+    #0 = ip
+    #1 = port
+    #2 = status
+    #3 = region
+    #4 = playercount
+    #5 = id
+	servers = [
+		{"0.0.0.0", "1", "ok", "usEast", 0, 1}
+	]
+    #TODO loop to get all servers and set their data in the array
+	return json.dumps(servers)
