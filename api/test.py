f59b173eb55b4d1cc455e84e5d4fa23103d085ce
vinnie <Vrinella4@gmail.com>
2024-09-18 07:59:51 -0400

    Update test.py

--- api/test.py
+++ api/test.py
@@ -9,9 +9,9 @@ def getServerList():
     #3 = region
     #4 = playercount
     #5 = id
-	servers = [
-		{"0.0.0.0", "1", "ok", "usEast", 0, 1}
-	]
+    servers = [
+        {"ip": "0.0.0.0", "port": 1, "status": "ok", "region": "usEast", "playercount": 0, "id": 1}
+    ]
     #TODO loop to get all servers and set their data in the array
 	return servers
 
