bae73e1d7d04e485fdc2e98a9bda804191656ae3
vinnie <Vrinella4@gmail.com>
2024-09-18 08:44:04 -0400

    test2

--- api/test.py
+++ api/test.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response
 import json
-from Endpoints.Servers import getServerList  # Correct import
+from Servers import getServerList  # Correct import
 
 
 app = Flask(__name__)
