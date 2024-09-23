bf7c81ab59efacfe85b685e8d3eda88923469b26
vinnie <Vrinella4@gmail.com>
2024-09-23 10:41:43 -0400

    Update API.py

--- api/API.py
+++ api/API.py
@@ -113,8 +113,8 @@ def addMachine(data):
     ids = ""
     servers = getServerList()
     for i in servers:
-        if(i['id'] in serverIds):
-            ids += str(i['id']) + "&"
+        if(i.__getitem__("id") in serverIds):
+            ids += str(i.__getitem__('id')) + "&"
             ids.rstrip("&")
     with open('/tmp/Servers.txt', 'w') as file:
         file.write(f"{data['ip']},{data['region']},{data['serverCount']},{data['status']},{data['id']},{ids}\n")
