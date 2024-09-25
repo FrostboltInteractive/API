e2b67d559b9911c7b64902dcdcf6a38f5bdedb7d
vinnie <Vrinella4@gmail.com>
2024-09-25 09:23:00 -0400

    Update API.py

--- api/API.py
+++ api/API.py
@@ -17,7 +17,7 @@ def getServerList():
 
     for line in arr:
         line = str(line)
-        arr = line.split('?')
+        arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7] = line.split('?')
         ip = arr[0]
         port = arr[1]
         status = arr[2]
