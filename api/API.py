ef2f5211b1bb617b6bdc5504d54b4e4c75c37c4d
vinnie <Vrinella4@gmail.com>
2024-12-04 12:09:44 -0500

    Update API.py

--- api/API.py
+++ api/API.py
@@ -113,14 +113,14 @@ def stopServer(id):
 def getMachineList():
     machines= []
     data = dataGet("Machines")
-    print(data)
     if(data is None):
         return ["error"]
     for line in data:
         arr = line.split(',')
         if(len(arr) < 7):
             continue
-        else:
+        else:   
+            print("Test")
             strs = line.strip().split(',')
             ip = strs[0]
             port = strs[1]
